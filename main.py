9ee71a4591f5cb93105b1bdad707fd9f1753e090
axeld5 <axel.darmouni@student-cs.fr>
2023-02-03 08:28:47 +0100

    added args for viz

--- main.py
+++ main.py
@@ -7,36 +7,45 @@ from models.transformer import TransformerMT
 from models.lstm import LSTMMT
 from models.lstm_utils import normalizeString
 from bleu_metric import sacrebleu_metric
-from visualise_results import visualize
+from visualise_results import visualize, show_boxplot
 
 print(torch.cuda.is_available())
 if __name__ == "__main__":
     dataset = load_dataset("tatoeba", lang1 = "en", lang2 = "fr", split=ReadInstruction("train",from_=0, to=100, unit="%", rounding="pct1_dropremainder"))
+    perf_dict = {"model_name":[], "sacrebleu":[]}
+    for _ in range(1):
+        lstm_model = LSTMMT(hidden_size=1024, max_length=500)
+        lstm_model.build_language(dataset)
+        lstm_model.init_models()
+        dataset = dataset.train_test_split(test_size=0.01)
+        test_dataset = dataset["test"]
+        lstm_model.load_model()
+        
+        test = []   
+        normalized_test = []    
+        fr_test = []
+        fr_normalized_test = []
+        for i in range(len(test_dataset)):
+            elem = test_dataset[i] 
+            test.append(elem["translation"]["en"])        
+            normalized_test.append(normalizeString(elem["translation"]["en"]))
+            fr_test.append(elem["translation"]["fr"])
+            fr_normalized_test.append(normalizeString(elem["translation"]["fr"]))
+        lstm_predictions = lstm_model.predict(normalized_test)
+        
+        lstm_score = sacrebleu_metric(fr_normalized_test, lstm_predictions)
+        perf_dict["model_name"].append("s2s_lstm")
+        perf_dict["sacrebleu"].append(lstm_score["bleu"])
 
-    lstm_model = LSTMMT(hidden_size=1024, max_length=500)
-    lstm_model.build_language(dataset)
-    lstm_model.init_models()
-    dataset = dataset.train_test_split(test_size=0.01)
-    test_dataset = dataset["test"]
-    lstm_model.load_model()
-    
-    test = []
-    
-    for i in range(len(test_dataset)):
-        test.append(test_dataset[i]["translation"]["en"])
-    normalized_test = []    
-    for i in range(len(test_dataset)):
-        normalized_test.append(normalizeString(test_dataset[i]["translation"]["en"]))
-    lstm_predictions = lstm_model.predict(normalized_test)
-    
-    lstm_score = sacrebleu_metric(normalized_test, lstm_predictions)
-    print(lstm_score)
+        transformer_model = TransformerMT()
+        transformer_model.load_model()
 
-    transformer_model = TransformerMT()
-    transformer_model.load_model()
+        transformer_predictions = transformer_model.predict(test)
+        transformer_score = sacrebleu_metric(fr_test, transformer_predictions)
+        perf_dict["model_name"].append("hugface_transformer")
+        perf_dict["sacrebleu"].append(transformer_score["bleu"])
 
-    transformer_predictions = transformer_model.predict(test)
-    transformer_score = sacrebleu_metric(test, transformer_predictions)
-    perf_dict = {"s2s_lstm": lstm_score['bleu'], "hugface_transformer":transformer_score['bleu']}
-    visualize(perf_dict)
+    #perf_dict = {"s2s_lstm": lstm_score['bleu'], "hugface_transformer":transformer_score['bleu']}
+    #visualize(perf_dict)
+    show_boxplot(perf_dict, x="model_name", y="sacrebleu")
     
--- visualise_results.py
+++ visualise_results.py
@@ -14,6 +14,12 @@ def visualize(model_perf:Dict[str, float]) -> None:
     ax.set_title("Bleu score for each model")
     plt.show() 
 
+def show_boxplot(model_dict:Dict[str, float], x:str, y:str, how:str) -> None:
+    df = pd.DataFrame.from_dict(model_dict)
+    sns.boxplot(x=x, y=y, data=df)
+    plt.show()
+
+
 if __name__ == "__main__":
     model_perf = {"LSTM":35, "ConvS2S":37, "Transformer":40}
     visualize(model_perf)
